bd8996ed4a4ce344bed0cd42da683dda6471016c
anakdrch <ana.kudrevich@gmail.com>
2025-02-28 20:06:47 +0100

    feat: implement carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const sampleActivityNum = Number(sampleActivity);
+  if (sampleActivity && typeof sampleActivity === 'string' && sampleActivityNum > 0 && sampleActivityNum <= 15) {
+    const rateK = 0.693 / HALF_LIFE_PERIOD;
+    const age = Math.log(MODERN_ACTIVITY / sampleActivityNum) / rateK;
+    return Math.ceil(age);
+  } 
+  return false;
 }
 
 module.exports = {
