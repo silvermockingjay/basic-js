e7bedbce0fc66dd1b69b09c62801443b533b8b6e
anakdrch <ana.kudrevich@gmail.com>
2025-03-01 21:16:47 +0100

    feat: implement extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let addition = [];
+  let repeatedAddition;
+  let stringWithAddition = str;
+  let finalStringArray = [];
+  let ans;
+
+  options.additionSeparator = options.additionSeparator || '|';
+  options.separator = options.separator || '+';
+
+  if (typeof str !== 'string') {
+    str = String(str);
+  }
+
+  if (options.hasOwnProperty("addition")) {
+    options.addition = String(options.addition);
+    if (options.additionRepeatTimes) {
+      for (let i = 0; i < options.additionRepeatTimes; i++) {
+        addition.push(options.addition);
+      }
+      repeatedAddition = addition.join(options.additionSeparator);
+      stringWithAddition += repeatedAddition;
+    } else {
+      stringWithAddition += options.addition; 
+    }
+  }
+
+  if (options.repeatTimes) {
+    for (let i = 0; i < options.repeatTimes; i++) {
+      finalStringArray.push(stringWithAddition);
+    }
+    ans = finalStringArray.join(options.separator);
+  } else {
+    ans = stringWithAddition;
+  }
+  return ans;
 }
 
 module.exports = {
